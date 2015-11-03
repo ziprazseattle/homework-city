8788633404a70e99b663851bc88d21e1c0b911df
ziprazseattle <ziprazseattle@yahoo.com>
2015-11-02 18:32:57 -0800

    pseudo code - js not responding
    Just barely got started on the homework. Looks like I’m not properly
    linked up between html, css and js files.

---
+++ starter_code/js/index-info.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+
+/*
+
+function myFunction() {
+    alert("Hello!");
+}
+*/
+	
+
+	function question1(){
+		$("h1").css("color", "blue");
+	}
+
+	$(h1).click(question1);
+		
+
+/*
+
+var = "city"
+
+//when user submits a var ("city") 
+//don't do the usual submit behavior 
+//get the variable? val 
+
+
+$("").on(submit) 
+	event.preventDefault();
+	e.preventDefault();
+
+
+//if var is New York, New York City, or NY
+//then change the bkgd img to nyc.jpg 
+	
+	if city == "New York" || "New York City" || "NY"
+		$("body").css("background-image", "nyc.jpg")
+
+//else if var is San Francisco, Bay Area, or SF
+//then change bkgd img to sf.jpg
+	else if city == "San Francisco" || "Bay Area" || "SF"
+		$("body").css("background-image", "nyc.jpg")
+
+//else if var is Los Angeles, LAX, or LA 
+//then change bkgd img to la.jpg
+
+//else if var is Sydney or SYD
+//then change bkgd img to sydney 
+
+//else if var none of the above
+//display alert ("We don't have pictures for that city yet")
+//"Upload picture" 
+
+	else alert("We don't have pictures for that city yet. <a href="#">Upload a picture</a>")
+*/ 
+
+//end doc ready 
+});
